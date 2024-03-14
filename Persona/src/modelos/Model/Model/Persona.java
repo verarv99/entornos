7f2dd68b6d410dd98314483da7ae60e833bc50ef
verarv99 <veravr99@gmail.com>
2024-03-14 14:28:14 +0100

    persona subido

---
+++ Persona/src/modelos/Model/Model/Persona.java
@@ -0,0 +1,65 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private int dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido1;
+
+    /**
+     * 
+     */
+    private String apellido2;
+
+    /**
+     * 
+     */
+    public Int edad = 0;
+
+    /**
+     * @param Nombre 
+     * @param apellido1 
+     * @param dni 
+     * @param apellido2 
+     * @param edad
+     */
+    public void Persona(string Nombre, String apellido1, string dni, string apellido2, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void sett dni(string dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Parameter1
+     */
+    public void getDni(void Parameter1) {
+        // TODO implement here
+    }
+
+}
---
+++ Persona/src/modelos/Model/Persona.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private int dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido1;
+
+    /**
+     * 
+     */
+    private String apellido2;
+
+    /**
+     * 
+     */
+    public Int edad = 0;
+
+    /**
+     * @param Nombre 
+     * @param apellido1 
+     * @param dni 
+     * @param apellido2 
+     * @param edad
+     */
+    public void Persona(string Nombre, String apellido1, string dni, string apellido2, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void sett dni(string dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+}
