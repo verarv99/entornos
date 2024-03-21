0b159d54e4d02a26fc3df6ac76989489167ce697
verarv99 <veravr99@gmail.com>
2024-03-21 09:45:03 +0100

    Clase casa creada

---
+++ Personas/casa.java
@@ -0,0 +1,92 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class casa {
+
+    /**
+     * Default constructor
+     */
+    public casa() {
+    }
+
+    /**
+     * 
+     */
+    private String direccion;
+
+    /**
+     * 
+     */
+    private Int nºhabitaciones;
+
+    /**
+     * 
+     */
+    private Int metroscuadrados;
+
+    /**
+     * @param Direccion 
+     * @param habitaciones 
+     * @param Metros
+     */
+    public void casa(String Direccion, Int habitaciones, Int Metros) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public Stirng GetDIreccion() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public Int Gethabitaciones() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public Int GetMetroscuadrados() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param Direccion
+     */
+    public void SetDireccion(String Direccion) {
+        // TODO implement here
+    }
+
+    /**
+     * @param nºhabitaciones
+     */
+    public void SetnºHabitaciones(Int nºhabitaciones) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Metros
+     */
+    public void SetMetros(Int Metros) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String ToString() {
+        // TODO implement here
+        return "";
+    }
+
+}
